0965fb5d241bbc84c32f97084d1693f2e14b94f1
Bpost129 <Bpost129@yahoo.com>
2024-08-02 18:46:01 -0400

    switch case with terminal input

--- Main.java
+++ Main.java
@@ -1,28 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+  public static Scanner scanner = new Scanner(System.in);
   public static void main(String[] args) {
-    String name = "Billy";
+    System.out.println("What time is it? ");
+    int time = scanner.nextInt();
 
-    // System.out.println("Hello " + name + "!");
-
-    
-    byte maxByte = Byte.MAX_VALUE;
-    byte minByte = Byte.MIN_VALUE;
-    
-    System.out.println("The max value for a byte is: " + maxByte);
-    System.out.println("The min value for a byte is: " + minByte);
-    
-    int max = Integer.MAX_VALUE;
-    int min = Integer.MIN_VALUE;
-
-    System.out.println("The max value for an integer is: " + max);
-    System.out.println("The min value for an integer is: " + min);
-    
-    long maxLong = Long.MAX_VALUE;
-    long minLong = Long.MIN_VALUE;
-    
-    System.out.println("The max value for a long is: " + maxLong);
-    System.out.println("The min value for a long is: " + minLong);
-    
-
+    switch (time) {
+      case 10:
+        System.out.println("The time is " + time + "\nTime for a coffee.");
+        break;
+      case 12:
+        System.out.println("The time is " + time + "\nTime for a run!");
+        break;
+      case 7:
+        System.out.println("The time is " + time + "\nTime to wake up.");
+        break;
+      default:
+        System.out.println("Time to relax.");
+        break;
+    }
   }
 }
