4a434ce371c8799f36e4ae2d86342c5c2d8826d6
Bpost129 <Bpost129@yahoo.com>
2024-08-02 19:05:46 -0400

    standard for loop

--- Main.java
+++ Main.java
@@ -3,22 +3,30 @@ import java.util.Scanner;
 public class Main {
   public static Scanner scanner = new Scanner(System.in);
   public static void main(String[] args) {
-    System.out.println("What time is it? ");
-    int time = scanner.nextInt();
+    // System.out.println("What time is it? ");
+    // int time = scanner.nextInt();
 
-    switch (time) {
-      case 10:
-        System.out.println("The time is " + time + "\nTime for a coffee.");
-        break;
-      case 12:
-        System.out.println("The time is " + time + "\nTime for a run!");
-        break;
-      case 7:
-        System.out.println("The time is " + time + "\nTime to wake up.");
-        break;
-      default:
-        System.out.println("Time to relax.");
-        break;
+    // switch (time) {
+    //   case 10:
+    //     System.out.println("The time is " + time + "\nTime for a coffee.");
+    //     break;
+    //   case 12:
+    //     System.out.println("The time is " + time + "\nTime for a run!");
+    //     break;
+    //   case 7:
+    //     System.out.println("The time is " + time + "\nTime to wake up.");
+    //     break;
+    //   default:
+    //     System.out.println("Time to relax.");
+    //     break;
+    // }
+
+    for (int i = 0; i <= 10; i++) {
+      System.out.println("i = " + i);
+      
     }
+
+
+
   }
 }
