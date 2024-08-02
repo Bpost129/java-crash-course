a659b65f34a1c6ae0ec2fe81434f3c929304dc67
Bpost129 <Bpost129@yahoo.com>
2024-08-01 23:57:16 -0400

    experiment with some data types

--- Main.java
+++ Main.java
@@ -1,6 +1,28 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello billy!");
+    String name = "Billy";
+
+    // System.out.println("Hello " + name + "!");
+
+    
+    byte maxByte = Byte.MAX_VALUE;
+    byte minByte = Byte.MIN_VALUE;
+    
+    System.out.println("The max value for a byte is: " + maxByte);
+    System.out.println("The min value for a byte is: " + minByte);
+    
+    int max = Integer.MAX_VALUE;
+    int min = Integer.MIN_VALUE;
+
+    System.out.println("The max value for an integer is: " + max);
+    System.out.println("The min value for an integer is: " + min);
+    
+    long maxLong = Long.MAX_VALUE;
+    long minLong = Long.MIN_VALUE;
+    
+    System.out.println("The max value for a long is: " + maxLong);
+    System.out.println("The min value for a long is: " + minLong);
+    
 
   }
 }
