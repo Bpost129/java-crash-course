c3a6bf99b824206ce86cc5cc20665b12ab79882b
Bpost129 <Bpost129@yahoo.com>
2024-08-14 23:55:15 -0400

    create Car class

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car {
+  private String name;
+  private String model; 
+  private String color;
+  private int doors;
+
+  public Car(String name, String model, String color, int doors) {
+    this.name = name;
+    this.model = model;
+    this.color = color;
+    this.doors = doors;
+  }
+}
